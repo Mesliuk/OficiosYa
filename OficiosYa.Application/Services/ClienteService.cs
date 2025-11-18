f7a689b7cb902147b37a95001981657931fda6a9
Mesliuk <82001197+Mesliuk@users.noreply.github.com>
2025-11-18 20:19:26 -0300

    Refactorizando Estructura

--- OficiosYa.Application/Services/ClienteService.cs
+++ OficiosYa.Application/Services/ClienteService.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OficiosYa.Infrastructure
+namespace OficiosYa.Application.Services
 {
     public class ClienteService
     {
