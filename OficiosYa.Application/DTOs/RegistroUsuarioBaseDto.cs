0c64f0d2ee4e129fafa07f799ff087c37ff9af35
Mesliuk <82001197+Mesliuk@users.noreply.github.com>
2025-12-06 19:11:12 -0300

    merge complete

--- OficiosYa.Application/DTOs/RegistroUsuarioBaseDto.cs
+++ OficiosYa.Application/DTOs/RegistroUsuarioBaseDto.cs
@@ -7,7 +7,7 @@ namespace OficiosYa.Application.DTOs
         public string Email { get; set; } = null!;
         public string Telefono { get; set; } = null!;
         public string Password { get; set; } = null!;
-        public string Direccion { get; set; } = null;
+        
 
     }
 }
